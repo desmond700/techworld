d63d35bda32c2310c216231654e32cf3a62d6cb0
desmond700 <dwdeveloper4@gmail.com>
2018-07-26 21:19:44 -0400

    added functionality to hide/show categories component

--- src/app/product-view.service.ts
+++ src/app/product-view.service.ts
@@ -1,16 +1,31 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { of } from 'rxjs/Observable/of';
+import { Observable } from 'rxjs/Observable';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductViewService {
 
-  private productsUrl = 'http://localhost:3000/api/products/';
+  private productsUrl = 'http://localhost:3000/api/product/';
+  private theBoolean: BehaviorSubject<boolean>;
 
-  constructor(private _http: HttpClient) { }
+  constructor(private _http: HttpClient) {
+    this.theBoolean = new BehaviorSubject<boolean>(true);
+  }
 
   getproductbyid(id) {
     return this._http.get(this.productsUrl + id);
+  }
+
+  isShowCategory(): Observable<boolean> {
+    return this.theBoolean.asObservable();
+  }
+  
+  public showCategory(newValue: boolean): void {
+    this.theBoolean.next(newValue);
   }
 }
