62d0812b20c6437113e0522a815df5704a15d458
desmond700 <dwdeveloper4@gmail.com>
2018-07-27 11:28:00 -0400

    removed unused code

--- src/app/categories/categories.component.ts
+++ src/app/categories/categories.component.ts
@@ -1,23 +1,11 @@
 import { Component, OnInit, EventEmitter, Output, HostBinding } from '@angular/core';
-import { CategoryProductsService } from '../category-products/category-products.service';
 
 @Component({
   selector: 'app-categories',
   templateUrl: './categories.component.html',
   styleUrls: ['./categories.component.css']
 })
-export class CategoriesComponent implements OnInit {
+export class CategoriesComponent {
 
-  @HostBinding('class.is-open')
-  isOpen = false;
-  
-  constructor(private _products: CategoryProductsService) { }
-
-  ngOnInit() {
-    this._products.change.subscribe(isOpen => {
-      this.isOpen = isOpen;
-    });
-  }
-  
-
+  constructor() {}
 }
