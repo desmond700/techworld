f64bccc14ad9b2aaa2f1dd41954d9cacaba8ceb2
desmond700 <dwdeveloper4@gmail.com>
2018-07-26 21:34:57 -0400

    subscribe to function from service

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef  } from '@angular/core';
+import { ProductViewService } from '../product-view.service';
 
 
 @Component({
@@ -8,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductListComponent implements OnInit {
 
-  constructor() {}
+    show: any;
 
-  ngOnInit() {
-  }
+    constructor(private _product: ProductViewService,
+                private cdr: ChangeDetectorRef) {
+    }
 
+    ngOnInit() {
+        this._product.isShowCategory().subscribe( res => {
+                this.show = res;
+                this.cdr.detectChanges();
+            }
+        );
+    }
 }
