5f6100708441beea44e47f454e7e81043ba525bc
desmond700 <dwdeveloper4@gmail.com>
2018-07-26 21:26:36 -0400

    added service

--- src/app/category-products/category-products.component.ts
+++ src/app/category-products/category-products.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, HostListener } from '@angular/core';
 import { CategoryProductsService } from './category-products.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { ProductViewService } from '../product-view.service';
 
 @Component({
   selector: 'app-category-products',
@@ -14,6 +15,7 @@ export class CategoryProductsComponent implements OnInit {
   category: any;
 
   constructor(private _products: CategoryProductsService,
+              private _product: ProductViewService,
               private route: ActivatedRoute,
               private router: Router) { }
 
@@ -28,6 +30,8 @@ export class CategoryProductsComponent implements OnInit {
         err => console.log('error retrieving object: ' + err)
       );
     });
+
+  this._product.showCategory(true);
   }
 
   productView(category) {
