777dabffe88a95272255c946b2e44c7f6943e01a
desmond700 <dwdeveloper4@gmail.com>
2018-07-26 21:34:12 -0400

    removed unused code

--- src/app/category-products/category-products.service.ts
+++ src/app/category-products/category-products.service.ts
@@ -7,20 +7,11 @@ import { HttpClient } from '@angular/common/http';
 export class CategoryProductsService {
 
   private productUrl = 'http://localhost:3000/api/products/';
-  @Output() productsObj: EventEmitter<any> = new EventEmitter();
-  @Output() change: EventEmitter<boolean> = new EventEmitter();
   result: any;
-  isOpen = false;
-  
 
   constructor(private _http: HttpClient) { }
 
   getProductObj(category) {
     return this._http.get( this.productUrl + category );
-  }
-
-  toggle() {
-    this.isOpen = !this.isOpen;
-    this.change.emit(this.isOpen);
   }
 }
