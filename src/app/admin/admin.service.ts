98434634a84d15b786b4a1c755b7d9085c6d2a23
desmond700 <dwdeveloper4@gmail.com>
2018-07-30 10:54:15 -0400

    modified url variable etc

--- src/app/admin/admin.service.ts
+++ src/app/admin/admin.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 import { Laptop } from './class/laptop';
 
 @Injectable({
@@ -7,16 +8,34 @@ import { Laptop } from './class/laptop';
 })
 export class AdminService {
 
-  private productUrl = 'http://localhost:3000/api';
+  private baseUrl = 'http://localhost:3000/api';
   result: any;
 
   constructor(private _http: HttpClient) { }
 
-  getProductObj(category) {
-    return this._http.get( `${this.productUrl}/products/` + category );
+  login(username: string, password: string) {
+    return this._http.post<any>(`${this.baseUrl}/admin/login`, { username: username, password: password })
+        .pipe(map(admin => {
+            // login successful if there's a jwt token in the response
+            if (admin.username !== null) {
+                // store user details and jwt token in local storage to keep user logged in between page refreshes
+                localStorage.setItem('admin', admin.username);
+            }
+
+            return admin;
+        }));
   }
-  
+
+  logout() {
+      // remove user from local storage to log user out
+      localStorage.removeItem('admin');
+  }
+
+  getProductObj(category) {
+    return this._http.get( `${this.baseUrl}/products/` + category );
+  }
+
   addProduct(laptop: Laptop) {
-      return this._http.post(`${this.productUrl}/product/laptop`, laptop);
+      return this._http.post(`${this.baseUrl}/product/laptop`, laptop);
   }
 }
