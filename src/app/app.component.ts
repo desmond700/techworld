5ffa5de4a4988d2789e24c4b2247a8475177fbea
desmond700 <dwdeveloper4@gmail.com>
2018-07-31 18:28:32 -0400

    adding functions for user purposes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { SlimLoadingBarService } from 'ng2-slim-loading-bar';
+import { AuthenticationService } from 'src/app/auth/auth.service';
 import { NavigationCancel,
 	Event,
 	NavigationEnd,
@@ -7,18 +8,33 @@ import { NavigationCancel,
 	NavigationStart,
 	Router } from '@angular/router';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  user: any;
 
-  constructor(private _loadingBar: SlimLoadingBarService, private _router: Router){
+  constructor(private _loadingBar: SlimLoadingBarService,
+			  private _router: Router,
+		  	  private authenticationService: AuthenticationService) {
+
 	  this._router.events.subscribe((event: Event) => {
 		  this.navigationInterceptor(event);
 	  });
+
+	  this.authenticationService.getUser().subscribe(
+		  res => { this.user = res; }
+	  );
+	  console.log(this.user);
+  }
+
+  logout() {
+	  this.authenticationService.logout();
+	  
+	  this._router.navigate(['/']);
   }
 
   private navigationInterceptor(event: Event): void {
