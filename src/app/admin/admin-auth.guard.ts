0eccf79b2472853b6973fd655638d735abdb588d
desmond700 <dwdeveloper4@gmail.com>
2018-08-01 11:32:39 -0400

    modified code for  better comparison

--- src/app/admin/admin-auth.guard.ts
+++ src/app/admin/admin-auth.guard.ts
@@ -9,12 +9,11 @@ export class AdminAuthGuard implements CanActivate {
 	
   constructor(private router: Router, private _route: ActivatedRoute) {}
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    if (localStorage.getItem('admin')) {
+    if (localStorage.getItem('currentUser') && (localStorage.getItem('userType') === 'admin')) {
 			console.log('logged in');
 		// logged in so return true
 		return true;
 	}
-	console.log(this._route);
 	// not logged in so redirect to login page with the return url
 	this.router.navigate(['admin', 'login'], {relativeTo: this._route});
 	return false;
