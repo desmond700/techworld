01df52e139f75260ba1185ddbc07bd304fe54f53
desmond700 <dwdeveloper4@gmail.com>
2018-08-01 11:33:24 -0400

    removed login and logout function

--- src/app/admin/admin.service.ts
+++ src/app/admin/admin.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators';
 import { Laptop } from './class/laptop';
 
 @Injectable({
@@ -12,24 +11,6 @@ export class AdminService {
   result: any;
 
   constructor(private _http: HttpClient) { }
-
-  login(username: string, password: string) {
-    return this._http.post<any>(`${this.baseUrl}/admin/login`, { username: username, password: password })
-        .pipe(map(admin => {
-            // login successful if there's a jwt token in the response
-            if (admin.username !== null) {
-                // store user details and jwt token in local storage to keep user logged in between page refreshes
-                localStorage.setItem('admin', admin.username);
-            }
-
-            return admin;
-        }));
-  }
-
-  logout() {
-      // remove user from local storage to log user out
-      localStorage.removeItem('admin');
-  }
 
   getProductObj(category) {
     return this._http.get(`${this.baseUrl}/products/` + category );
